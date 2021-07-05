467eb3497c2c1e4fe7e57498ec39cce16e61812a
Jesse Brink <jbrink0925@gmail.com>
2021-07-05 01:52:12 -0400

    initial setup of base template and board

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, session, url_for, request
+from decouple import config
 
 from boggle import Boggle
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 boggle_game = Boggle()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    classes = ['one', 'two', 'three', 'four', 'five']
+    if request.method == 'GET':
+        return render_template('home.html', board=boggle_game.make_board(),
+        classes=classes)
